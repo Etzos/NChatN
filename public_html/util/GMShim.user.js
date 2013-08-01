4461d4a1220173acba2c4e105c2d9f02a62030bd
Kevin Ott <supercodingmonkey@gmail.com>
2013-08-01 03:26:41 -0400

    Add a placeholder shim for this version
    Add a placeholder shim to point to the new shim update location. This
    placeholder uses version 1.7.2 to be newer than the last release, but
    older than the current one.

---
+++ public_html/util/GMShim.user.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name         NChatN Shim Loader
+// @namespace    http://garth.web.nowhere-else.org/web/
+// @version      1.7.2
+// @description  Loads NChatN (NEaB Chat Next) instead of the default NEaB chat
+// @match        http://www.nowhere-else.org/general_chat.php*
+// @match        http://nowhere-else.org/general_chat.php*
+// @match        http://*.nowhere-else.org/general_chat.php*
+// @require      https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js
+// @copyright    2013+, Kevin Ott
+// @resource     payload https://rawgithub.com/Etzos/NChatN/master/public_html/chat.html
+// @run-at       document-start
+// @updateURL    https://rawgithub.com/Etzos/NChatN/master/public_html/bin/GMShim.user.js
+// ==/UserScript==
+
+alert("If you see this, it means something went wrong. Please force an update for the NChatNext GMShim.user.js");
