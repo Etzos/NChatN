af6ad984359584d285c9a4011f3009b69617983d
Kevin Ott <supercodingmonkey@gmail.com>
2013-12-09 23:52:44 -0500

    Bump version for release (2.0.1a -> 2.0.2)

--- public_html/chat.js
+++ public_html/chat.js
@@ -14,7 +14,7 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 var Chat = (function(window, $) {
-    var version = "2.0.1a";
+    var version = "2.0.2";
 
     var URL = {
       'send': 'sendchat.php',
