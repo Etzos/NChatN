f742968b4ae2067c1eecafd5f3d8953777b96fe0
Kevin Ott <supercodingmonkey@gmail.com>
2014-07-10 02:26:31 -0400

    Make the Download Chat link stop bubbling

--- public_html/chat.js
+++ public_html/chat.js
@@ -1612,6 +1612,7 @@ var Chat = (function (window, $) {
 
                         downloadRaw(page);
                     });
+                    return false;
                 }
             },
             updateOnline: {
