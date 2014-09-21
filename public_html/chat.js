447f317945febb529eca45538c556775ef8481f3
Kevin Ott <supercodingmonkey@gmail.com>
2014-09-21 14:27:30 -0400

    Bump version number (2.3.3 -> 2.4)

--- public_html/chat.js
+++ public_html/chat.js
@@ -14,7 +14,7 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 var Chat = (function (window, $) {
-    var version = "2.3.3";
+    var version = "2.4";
 
     var URL = {
         send: 'sendchat.php',
